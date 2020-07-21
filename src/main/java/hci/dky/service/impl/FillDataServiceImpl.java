0619d444881d44dd4542c3df4a0a3cc6b1eda4e1
Jiaxin77 <905284679@qq.com>
2020-07-21 16:12:39 +0800

    获取sus量表答案

--- src/main/java/hci/dky/service/impl/FillDataServiceImpl.java
+++ src/main/java/hci/dky/service/impl/FillDataServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -119,16 +120,15 @@ public class FillDataServiceImpl implements FillDataService {
 
         AssessAndPlan assessAndPlan = assessAndPlanMapper.selectByPrimaryKey((long)planId);
 
-        for(Object oneAnswer : answer)
-        {
+        for(Object oneAnswer : answer) {
             Map answerInfo = (Map) oneAnswer;
-            int taskId = (int)answerInfo.get("taskId");
-            int expertId = (int)answerInfo.get("expertId");
-            int questionId = (int)answerInfo.get("questionId");
-            int score = (int)answerInfo.get("score");
+            int taskId = (int) answerInfo.get("taskId");
+            int expertId = (int) answerInfo.get("expertId");
+            int questionId = (int) answerInfo.get("questionId");
+            int score = (int) answerInfo.get("score");
 
-            ExpertTask expertTask = expertTaskMapper.selectByPrimaryKey((long)taskId);
-            Expert expert = expertMapper.selectByPrimaryKey((long)expertId);
+            ExpertTask expertTask = expertTaskMapper.selectByPrimaryKey((long) taskId);
+            Expert expert = expertMapper.selectByPrimaryKey((long) expertId);
 
             SusScaleAnswerPaper susScaleAnswerPaper;
             //查看有没有此答卷
@@ -136,19 +136,17 @@ public class FillDataServiceImpl implements FillDataService {
             SusScaleAnswerPaperExample.Criteria criteria = susScaleAnswerPaperExample.createCriteria();
             criteria.andTaskIdEqualTo(expertTask.getId()).andExpertIdEqualTo(expert.getId()).andPlanIdEqualTo(assessAndPlan.getId());
             List<SusScaleAnswerPaper> susScaleAnswerPapers = susScaleAnswerPaperMapper.selectByExample(susScaleAnswerPaperExample);
-           //如果没有，加上此份答卷
-            if(susScaleAnswerPapers.isEmpty()){
+            //如果没有，加上此份答卷
+            if (susScaleAnswerPapers.isEmpty()) {
                 susScaleAnswerPaper = new SusScaleAnswerPaper();
                 susScaleAnswerPaper.setExpertId(expert.getId());
                 susScaleAnswerPaper.setPlanId(assessAndPlan.getId());
                 susScaleAnswerPaper.setTaskId(expertTask.getId());
                 susScaleAnswerPaperMapper.insert(susScaleAnswerPaper);
-            }
-            else//如果有 第一项就是
+            } else//如果有 第一项就是
             {
                 susScaleAnswerPaper = susScaleAnswerPapers.get(0);
             }
-
 
 
             //看看有没有这道题的
@@ -158,35 +156,73 @@ public class FillDataServiceImpl implements FillDataService {
             List<SusScaleQuestionScore> susScaleQuestionScores = susScaleQuestionScoreMapper.selectByExample(susScaleQuestionScoreExample);
 
             //如果有 - 替换为新答案
-            if(!susScaleQuestionScores.isEmpty())
-            {
+            if (!susScaleQuestionScores.isEmpty()) {
                 SusScaleQuestionScore susScaleQuestionScore = susScaleQuestionScores.get(0);
                 susScaleQuestionScore.setScore((long) score);
                 susScaleQuestionScoreMapper.updateByPrimaryKey(susScaleQuestionScore);
 
             }
-                //如果没有 - 增加这道题的答案
+            //如果没有 - 增加这道题的答案
             else {
                 SusScaleQuestionScore susScaleQuestionScore = new SusScaleQuestionScore();
-                susScaleQuestionScore.setScore((long)score);
+                susScaleQuestionScore.setScore((long) score);
                 susScaleQuestionScore.setQuestionNumber((long) questionId);
                 susScaleQuestionScore.setSusPaperId(susScaleAnswerPaper.getId());
                 susScaleQuestionScoreMapper.insert(susScaleQuestionScore);
             }
-
-            //返回当前的答案列表；按照任务 - 用户 - 分数？
-
-
-
-
-
-
         }
 
+            //返回当前的答案列表；按照任务 - 用户 - 分数？
+        ArrayList<Object> allAnswers = new ArrayList<>();
+        SusScaleAnswerPaperExample susScaleAnswerPaperExample1 = new SusScaleAnswerPaperExample();
+        SusScaleAnswerPaperExample.Criteria criteria3 = susScaleAnswerPaperExample1.createCriteria();
+        criteria3.andPlanIdEqualTo(assessAndPlan.getId());List<SusScaleAnswerPaper> susScaleAnswerPaperList = susScaleAnswerPaperMapper.selectByExample(susScaleAnswerPaperExample1);
+
+
+        for(SusScaleAnswerPaper paper : susScaleAnswerPaperList)
+        {
+                HashMap<String,Object> AnswerPaper = new HashMap<>();
+                AnswerPaper.put("planId",paper.getPlanId());
+                AnswerPaper.put("taskId",paper.getTaskId());
+                AnswerPaper.put("expertId",paper.getExpertId());
+
+                ArrayList<Object> allScores = new ArrayList<>();
+                //所有题目的答案
+                SusScaleQuestionScoreExample susScaleQuestionScoreExample1 = new SusScaleQuestionScoreExample();
+                SusScaleQuestionScoreExample.Criteria criteria4 = susScaleQuestionScoreExample1.createCriteria();
+                criteria4.andSusPaperIdEqualTo(paper.getId());
+                List<SusScaleQuestionScore> susScaleQuestionScoreList = susScaleQuestionScoreMapper.selectByExample(susScaleQuestionScoreExample1);
+
+                for(SusScaleQuestionScore questionScore:susScaleQuestionScoreList)
+                {
+                    HashMap<String,Object> thisScore = new HashMap<>();
+                    thisScore.put("paperId",questionScore.getSusPaperId());
+                    thisScore.put("questionNum",questionScore.getQuestionNumber());
+                    thisScore.put("score",questionScore.getScore());
+                    allScores.add(thisScore);
+
+                }
+                AnswerPaper.put("scores",allScores);
+                allAnswers.add(AnswerPaper);
+        }
+
+        return ServerResponse.createBySuccess("填写成功",allAnswers);
 
 
 
+    }
 
+    @Override
+    @Transactional(propagation = Propagation.SUPPORTS)//增加事务回滚
+    public ServerResponse<List> getSusAnswer(int planId)
+    {
+        /**
+         * @Author jiaxin
+         * @Description 获取sus量表的答案（用于查看）//TODO
+         * @Date 11:30 上午 2020/7/20
+         * @Param [planId]
+         * @return hci.dky.common.ServerResponse<java.util.List>
+         **/
 
 
 
