9649a143044252b4e77f88a4968b3f284359d990
Jiaxin77 <905284679@qq.com>
2020-08-18 16:34:27 +0800

    修改并测试了提交sus答案、获取sus答案；

--- src/main/java/hci/dky/service/impl/SusServiceImpl.java
+++ src/main/java/hci/dky/service/impl/SusServiceImpl.java
@@ -81,8 +81,8 @@ public class SusServiceImpl implements SusService {
             Map answerInfo = (Map) oneAnswer;
             int taskId = (int) answerInfo.get("taskId");
             int expertId = (int) answerInfo.get("expertId");
-            int questionId = (int) answerInfo.get("questionId");
-            int score = (int) answerInfo.get("score");
+      //      int questionId = (int) answerInfo.get("questionId");
+      //      int score = (int) answerInfo.get("score");
 
             ExpertTask expertTask = expertTaskMapper.selectByPrimaryKey((long) taskId);
             Expert expert = expertMapper.selectByPrimaryKey((long) expertId);
@@ -105,28 +105,38 @@ public class SusServiceImpl implements SusService {
                 susScaleAnswerPaper = susScaleAnswerPapers.get(0);
             }
 
+            ArrayList<Object> questionAnswerList = (ArrayList<Object>) answerInfo.get("questionAnswer");
+            for(Object questionAnswer : questionAnswerList)
+            {
+                Map questionInfo = (Map) questionAnswer;
+                int questionId = (int) questionInfo.get("questionId");
+                int score = (int) questionInfo.get("score");
 
-            //看看有没有这道题的
-            SusScaleQuestionScoreExample susScaleQuestionScoreExample = new SusScaleQuestionScoreExample();
-            SusScaleQuestionScoreExample.Criteria criteria1 = susScaleQuestionScoreExample.createCriteria();
-            criteria1.andSusPaperIdEqualTo(susScaleAnswerPaper.getId()).andQuestionNumberEqualTo((long) questionId);
-            List<SusScaleQuestionScore> susScaleQuestionScores = susScaleQuestionScoreMapper.selectByExample(susScaleQuestionScoreExample);
+                //看看有没有这道题的
+                SusScaleQuestionScoreExample susScaleQuestionScoreExample = new SusScaleQuestionScoreExample();
+                SusScaleQuestionScoreExample.Criteria criteria1 = susScaleQuestionScoreExample.createCriteria();
+                criteria1.andSusPaperIdEqualTo(susScaleAnswerPaper.getId()).andQuestionNumberEqualTo((long) questionId);
+                List<SusScaleQuestionScore> susScaleQuestionScores = susScaleQuestionScoreMapper.selectByExample(susScaleQuestionScoreExample);
 
-            //如果有 - 替换为新答案
-            if (!susScaleQuestionScores.isEmpty()) {
-                SusScaleQuestionScore susScaleQuestionScore = susScaleQuestionScores.get(0);
-                susScaleQuestionScore.setScore((long) score);
-                susScaleQuestionScoreMapper.updateByPrimaryKey(susScaleQuestionScore);
+                //如果有 - 替换为新答案
+                if (!susScaleQuestionScores.isEmpty()) {
+                    SusScaleQuestionScore susScaleQuestionScore = susScaleQuestionScores.get(0);
+                    susScaleQuestionScore.setScore((long) score);
+                    susScaleQuestionScoreMapper.updateByPrimaryKey(susScaleQuestionScore);
+
+                }
+                //如果没有 - 增加这道题的答案
+                else {
+                    SusScaleQuestionScore susScaleQuestionScore = new SusScaleQuestionScore();
+                    susScaleQuestionScore.setScore((long) score);
+                    susScaleQuestionScore.setQuestionNumber((long) questionId);
+                    susScaleQuestionScore.setSusPaperId(susScaleAnswerPaper.getId());
+                    susScaleQuestionScoreMapper.insert(susScaleQuestionScore);
+                }
 
             }
-            //如果没有 - 增加这道题的答案
-            else {
-                SusScaleQuestionScore susScaleQuestionScore = new SusScaleQuestionScore();
-                susScaleQuestionScore.setScore((long) score);
-                susScaleQuestionScore.setQuestionNumber((long) questionId);
-                susScaleQuestionScore.setSusPaperId(susScaleAnswerPaper.getId());
-                susScaleQuestionScoreMapper.insert(susScaleQuestionScore);
-            }
+
+
         }
 
         //返回当前的答案列表；按照任务 - 用户 - 分数？
